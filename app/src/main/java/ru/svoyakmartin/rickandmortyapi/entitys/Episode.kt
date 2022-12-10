bd81853cddf61593815bac63d2873cf88d2b0914
SvoyakMartin <svoyakmartin@gmail.com>
2022-12-10 18:42:15 +0500

    add entitys from api

---
+++ app/src/main/java/ru/svoyakmartin/rickandmortyapi/entitys/Episode.kt
@@ -0,0 +1,11 @@
+package ru.svoyakmartin.rickandmortyapi.entitys
+
+data class Episode(
+    val id: Int,// The id of the episode.
+    val name: String,// The name of the episode.
+    val air_date: String,//	The air date of the episode.
+    val episode: String,//	The code of the episode.
+    val characters: List<String>,//	List of characters who have been seen in the episode.
+    val url: String,// Link to the episode's own endpoint.
+    val created: String// Time at which the episode was created in the database.
+)
---
+++ app/src/main/java/ru/svoyakmartin/rickandmortyapi/entitys/Location.kt
@@ -0,0 +1,11 @@
+package ru.svoyakmartin.rickandmortyapi.entitys
+
+data class Location(
+    val id: Int,// The id of the location.
+    val name: String,// The name of the location.
+    val type: String,// The type of the location.
+    val dimension: String,// The dimension in which the location is located.
+    val residents: List<String>,// List of character who have been last seen in the location.
+    val url: String,// Link to the location's own endpoint.
+    val created: String// Time at which the location was created in the database.
+)
