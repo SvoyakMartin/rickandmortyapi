e13aa2f68bad081e6efd14300b8d138a028e6620
SvoyakMartin <svoyakmartin@gmail.com>
2022-12-21 01:53:34 +0500

    add animatorSet

--- app/src/main/java/ru/svoyakmartin/rickandmortyapi/CharactersFragment.kt
+++ app/src/main/java/ru/svoyakmartin/rickandmortyapi/CharactersFragment.kt
@@ -1,6 +1,9 @@
 package ru.svoyakmartin.rickandmortyapi
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.*
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.animation.doOnEnd
 import ru.svoyakmartin.rickandmortyapi.databinding.FragmentCharacterBinding
 
 class CharactersFragment : Fragment() {
@@ -35,15 +39,47 @@ class CharactersFragment : Fragment() {
             repeatCount = 3
         }
 
+        val animatorSet = AnimatorSet()
+
         with(binding) {
+            val animator_r =
+                ObjectAnimator.ofFloat(buttonAnim, View.ROTATION, 0f, 120f, 80f, 95f)
+                    .apply {
+                        interpolator = AccelerateDecelerateInterpolator()
+                        duration = 5000
+                    }
+
+            val animator_t =
+                ObjectAnimator.ofFloat(buttonAnim, View.TRANSLATION_Y, 0f, 140f).apply {
+                    interpolator = BounceInterpolator()
+                    duration = 3000
+                }
+
+            buttonAnimTwo.setOnClickListener {
+                if (!animatorSet.isRunning) {
+                    animatorSet.apply {
+                        playTogether(animator_r, animator_t)
+                        doOnEnd {
+                                                    buttonAnim.apply {
+                                                        translationY = 0f
+                                                        rotation = 0f
+                                                    }
+
+                        }
+                        start()
+                    }
+                }
+            }
+
             buttonAnim.setOnClickListener {
-                if (!animator.isRunning){
+                if (!animator.isRunning) {
                     val currentY = it.y
                     animator.apply {
                         setFloatValues(0f, 100f, 0f, -100f, 0f)
                         addUpdateListener {
                             buttonAnim.y = currentY + animatedValue as Float
                         }
+                        doOnEnd { buttonAnimTwo.visibility = View.VISIBLE }
                     }.start()
                 }
             }
