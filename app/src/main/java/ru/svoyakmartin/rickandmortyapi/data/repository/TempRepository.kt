1a125c11ff7c95d034c98ddda30e6e1aca347c99
SvoyakMartin <svoyakmartin@gmail.com>
2023-01-10 13:49:17 +0500

    refactor

---
+++ app/src/main/java/ru/svoyakmartin/rickandmortyapi/data/repository/TempRepository.kt
@@ -0,0 +1,22 @@
+package ru.svoyakmartin.rickandmortyapi.data
+
+import ru.svoyakmartin.rickandmortyapi.domain.models.Character
+class TempRepository{
+    fun getExampleDataList(): ArrayList<Character> {
+        return arrayListOf(
+            Character(0, "Keara", "", "", "", "", "", "", "", listOf("The ABC's of Beth"), "", ""),
+            Character(1, "Sleepy Gary", "", "", "", "", "", "", "", listOf("Total Rickall"), "", ""),
+            Character(2, "Pussifer", "", "", "", "", "", "", "", listOf("Rickmurai Jack"), "", ""),
+            Character(3, "Keara", "", "", "", "", "", "", "", listOf("The ABC's of Beth"), "", ""),
+            Character(4, "Sleepy Gary", "", "", "", "", "", "", "", listOf("Total Rickall"), "", ""),
+            Character(5, "Pussifer", "", "", "", "", "", "", "", listOf("Rickmurai Jack"), "", ""),
+            Character(6, "Keara", "", "", "", "", "", "", "", listOf("The ABC's of Beth"), "", ""),
+            Character(7, "Sleepy Gary", "", "", "", "", "", "", "", listOf("Total Rickall"), "", ""),
+            Character(8, "Pussifer", "", "", "", "", "", "", "", listOf("Rickmurai Jack"), "", "")
+        )
+    }
+
+    fun getNewItem(index: Int): Character {
+        return Character(index, "ADD_$index", "", "", "", "", "", "", "", listOf("EPISODE_$index"), "", "")
+    }
+}
