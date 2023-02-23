e8fa9625d3008881218243bdf1de7fa1ba5c42de
SvoyakMartin <svoyakmartin@gmail.com>
2023-02-24 00:05:25 +0500

    match_parent to 0dp on ConstraintLayout

--- features/feature-character/src/main/java/ru/svoyakmartin/featureCharacter/data/ExportRepositoryImpl.kt
+++ features/feature-character/src/main/java/ru/svoyakmartin/featureCharacter/data/ExportRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package ru.svoyakmartin.featureCharacter.data
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import ru.svoyakmartin.coreNetwork.provider.response.ApiResponse
 import ru.svoyakmartin.featureCharacter.data.dataSource.CharactersApi
 import ru.svoyakmartin.featureCharacter.data.dataSource.getEpisodesIds
 import ru.svoyakmartin.featureCharacter.data.dataSource.toCharacter
@@ -26,10 +29,25 @@ class ExportRepositoryImpl @Inject constructor(
 
                 characterRoomDAO.getCharactersNameByIds(characterIdsList).collect { emit(it) }
             }
-    }
+    }.flowOn(Dispatchers.IO)
 
     private suspend fun fetchCharactersByIds(ids: String) {
         val response = apiService.getCharactersByIds(ids)
+//        if (response is ApiResponse.Success) {
+//            response.data.apply {
+//            val charactersList = ArrayList<Character>()
+//            val charactersAndEpisodesIdsMap = mutableMapOf<Int, List<Int>>()
+//
+//            forEach { characterDTO ->
+//                val character = characterDTO.toCharacter()
+//                charactersList.add(character)
+//                charactersAndEpisodesIdsMap[characterDTO.id] = characterDTO.getEpisodesIds()
+//            }
+//
+//            characterRoomDAO.insertCharacters(charactersList)
+//            characterDependenciesFeatureApi.insertCharactersAndEpisodes(charactersAndEpisodesIdsMap)
+//            }
+//        }
 
         response.body()?.apply {
             val charactersList = ArrayList<Character>()
