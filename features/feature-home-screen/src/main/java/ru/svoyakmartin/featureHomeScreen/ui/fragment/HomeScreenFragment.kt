2e3031c0f0d672b095816385bc22c194676d1d9a
SvoyakMartin <svoyakmartin@gmail.com>
2023-03-16 04:52:53 +0500

    fix click on active tab, add backPressed callback

--- features/feature-home-screen/src/main/java/ru/svoyakmartin/featureHomeScreen/ui/fragment/HomeScreenFragment.kt
+++ features/feature-home-screen/src/main/java/ru/svoyakmartin/featureHomeScreen/ui/fragment/HomeScreenFragment.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import ru.svoyakmartin.coreDi.di.dependency.findFeatureExternalDependencies
 import ru.svoyakmartin.coreUI.viewModel
@@ -17,28 +21,30 @@ import ru.svoyakmartin.featureHomeScreen.di.HomeScreenNavSource
 import ru.svoyakmartin.featureHomeScreen.ui.viewModel.HomeScreenComponentDependenciesProvider
 import ru.svoyakmartin.featureHomeScreen.ui.viewModel.HomeScreenComponentViewModel
 import javax.inject.Inject
+import kotlin.system.exitProcess
 
 class HomeScreenFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var navController: NavController
+
     @Inject
     lateinit var homeScreenNavSource: HomeScreenNavSource
-//    private var backPressed: Long = 0
-//    private val callback = object : OnBackPressedCallback(true) {
-//        override fun handleOnBackPressed() {
-//            backPressed()
-//        }
-//    }
+    private var backPressed: Long = 0
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            backPressed()
+        }
+    }
 
-//    override fun onResume() {
-//        super.onResume()
-//        callback.isEnabled = true
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        callback.isEnabled = false
-//    }
+    override fun onResume() {
+        super.onResume()
+        callback.isEnabled = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        callback.isEnabled = false
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -58,39 +64,47 @@ class HomeScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initViews()
-
         navController =
             (childFragmentManager.findFragmentById(R.id.home_fragment_container) as NavHostFragment).navController
         binding.homeFragmentBottomNavigation.setupWithNavController(navController)
 
-//        if (childFragmentManager.fragments.isEmpty()) {
-//            setFragment(characterFragment, CharacterFeatureApi.BACKSTACK_KEY)
-//        } else {
-//            setFragmentFromBottomMenu(binding.startFragmentBottomNavigation.selectedItemId)
-//        }
+        initViews()
     }
 
     private fun initViews() {
-//        with(requireActivity()) {
-//            onBackPressedDispatcher.addCallback(this, callback)
-//        }
+        binding.homeFragmentBottomNavigation.setOnItemSelectedListener {
+            if (binding.homeFragmentBottomNavigation.selectedItemId != it.itemId) {
+                NavigationUI.onNavDestinationSelected(it, navController)
+            }
+
+            true
+        }
+
+
+        with(requireActivity()) {
+            onBackPressedDispatcher.addCallback(this, callback)
+        }
     }
 
-//    private fun backPressed() {
-//        if (backPressed + 2000 > System.currentTimeMillis()) {
-//            requireActivity().apply {
-//                finishAffinity()
-//                exitProcess(0)
-//            }
-//        } else {
-//            Toast.makeText(
-//                requireActivity(),
-//                getString(R.string.back_pressed_toast_text),
-//                Toast.LENGTH_SHORT
-//            ).show()
-//
-//            backPressed = System.currentTimeMillis()
-//        }
-//    }
+    private fun backPressed() {
+        (navController as NavHostController).currentBackStackEntry
+        if (backPressed + 2000 > System.currentTimeMillis()) {
+            requireActivity().apply {
+                finishAffinity()
+                exitProcess(0)
+            }
+        } else {
+            if (navController.previousBackStackEntry == null) {
+                Toast.makeText(
+                    requireActivity(),
+                    getString(R.string.back_pressed_toast_text),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                backPressed = System.currentTimeMillis()
+            } else {
+                navController.popBackStack()
+            }
+        }
+    }
 }
